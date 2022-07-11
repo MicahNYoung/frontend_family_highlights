32743e0f268e964db665882cf2521e4159fbe32f
Micah <micahnyoung95@gmail.com>
2022-07-11 17:09:35 -0500

    broken LoginPage, state not being set to fetched response

--- src/components/webpages/LoginPage.js
+++ src/components/webpages/LoginPage.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container } from '@mui/system';
@@ -7,22 +7,26 @@ import { useNavigate } from 'react-router';
 
 
 export default function FormPropsTextFields() {
-    const [email, setEmail] = React.useState("")
-    const [firstName, setFirstName] = React.useState("");
-    const [lastName, setLastName] = React.useState("");
-    const [username, setUsername] = React.useState("");
-    const [password, setPassword] = React.useState("");
-    const [momFirstName, setMomFirstName] = React.useState("");
-    const [momLastName, setMomLastName] = React.useState("");
-    const [dadLastName, setDadLastName] = React.useState("");
-    const [dadFirstName, setDadFirstName] = React.useState("");
-    const [familyId, setFamilyId] = React.useState("");
-    const [familyName, setFamilyName] = React.useState("");
-    const [familyMember, setFamilyMember] = React.useState({})
+  const [email, setEmail] = React.useState("")
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [momFirstName, setMomFirstName] = React.useState("");
+  const [momLastName, setMomLastName] = React.useState("");
+  const [dadLastName, setDadLastName] = React.useState("");
+  const [dadFirstName, setDadFirstName] = React.useState("");
+  const [familyName, setFamilyName] = React.useState("");
+  const [familyMember, setFamilyMember] = React.useState({})
+  const [needFamilyId, setNeedFamilyId] = React.useState(false);
+  const [userLoggingIn, setUserLoggingIn] = React.useState({username:"", password:"" });
+  const [testState, setTestState] = React.useState("")
+  // const [searchParams, setSearchParams] = useSearchParams();
 
-    const [needFamilyId, setNeedFamilyId] = React.useState(false);
-    const [userLoggingIn, setUserLoggingIn] = React.useState({username:"", password:"" });
-    const [testState, setTestState] = React.useState("")
+
+  const [familyMemberId, setFamilyMemberId] = React.useState()
+  const [familyId, setFamilyId] = React.useState()
+  const [familyMemberIdSet, setFamilyMemberIdSet] = React.useState(false)
   
     const navigate = useNavigate();
 
@@ -38,30 +42,45 @@ export default function FormPropsTextFields() {
         }))
     }
 ////Uncertain why this to navigate('homepage') are both necessary. Seems like the fetch in useEffect should be good.
-    React.useEffect(() => {
-      fetch("http://localhost:8080/familymember/get?username=" + userLoggingIn.username)
-      .then(response=> response.json())
-      .then(data => {
-        console.log(data)
-        const familyMemberId = data.id;
-        console.log("familyMemberId is " + familyMemberId)
-        return fetch("http://localhost:8080/family?familyMemberId=" + familyMemberId);
-      })
-      .then(familyResponse => familyResponse.text())
-      .then(data => {
-        const familyId = data
-        console.log(familyId)
-      });
-      
-      console.log(familyId);
-      
-   
-    },[testState])
+React.useEffect(() => {
+  fetch("http://localhost:8080/familymember/get?username=" + userLoggingIn.username)
+  .then(response=> response.json())
+  .then(data => {
+    console.log(data)
+    // familyMemberId = data.id;
+    setFamilyMemberId(data.id);
+    // return fetch("http://localhost:8080/family?familyMemberId=" + familyMemberId.id);
+  })
+},[testState])
+
+React.useEffect(() => {
+  fetch("http://localhost:8080/family?familyMemberId=" + familyMemberId)
+  .then(familyResponse => familyResponse.text())
+  .then(data => {
+    console.log("data being returned from /family?familyMemberId= is "+  data)
+    // familyId = data;
+    setFamilyId(data);
+  })
+}, [familyMemberId])
+
+console.log("familyMemberId is: &&&&&&&&" + familyMemberId)
+    console.log("familyId is: &&&&&&&&&&" + familyId)
     
-    const handleClick =(e) =>{
-        e.preventDefault()
-        setTestState("test")
-        }
+const handleClick =(e) =>{
+    e.preventDefault()
+    setTestState("test");
+    console.log("familyMemberId is: ++++++++++" + familyMemberId)
+    console.log("familyId is: +++++++++++" + familyId)
+    setTimeout(() => {
+      
+      navigate({
+        pathname: '/homepage',
+        search: '?familyId=' + familyId,
+      })
+    }, 1000)
+
+    
+    }
        
     const handleNewFamily =(e)=>{
       setNeedFamilyId(true);
