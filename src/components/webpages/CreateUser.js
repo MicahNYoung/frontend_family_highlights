a9c1cccdb1040486e1747dfd79be48d0976b4f36
Micah <micahnyoung95@gmail.com>
2022-07-24 16:00:08 -0500

    added client-side form validation to CreateUser, still need to handle invalid familyId

--- src/components/webpages/CreateUser.js
+++ src/components/webpages/CreateUser.js
@@ -7,7 +7,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { NewFamily } from "../createuser_components/NewFamily";
 import Navbar from "../Navbar";
-import { ErrorMessage } from "../validation/ErrorMessage";
 const salt = bcrypt.genSaltSync(10);
 
 export function CreateUser({ page }) {
@@ -23,18 +22,46 @@ export function CreateUser({ page }) {
   const [familyId, setFamilyId] = useState("");
   const [familyName, setFamilyName] = useState("");
   const [needFamilyId, setNeedFamilyId] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
-  const [allFieldsFilled, setAllFieldsFilled] = useState(false);
   const [response, setResponse] = useState({ id: 0 });
-  const [errors, setErrors] = useState();
-  const [errorsArr, setErrorsArr] = useState();
+  // const [errors, setErrors] = useState();
 
+  const [hasError, setHasError] = useState(false);
   const navigate = useNavigate();
+  let errors = [];
+
+  function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  function getEmailValidationMessage(email) {
+    if (formSubmitted) {
+      if (email === "") {
+        // setHasError(true);
+        errors.push("error");
+        return "Field is blank!";
+      } else if (!isValidEmail(email)) {
+        return "invalid email!";
+      } else return false;
+    }
+  }
+  function getFieldValidationMessage(field) {
+    if (formSubmitted) {
+      if (field === "") {
+        // setHasError(true);
+        return "Field is blank!";
+      }
+    }
+  }
+
+  function getFamilyIdValidationMessage(familyId) {}
 
   const handleClick = (e) => {
     password = bcrypt.hashSync(password, salt);
-
     // e.preventDefault();
+    setFormSubmitted(true);
+
     const familyMember = {
       email,
       firstName,
@@ -49,40 +76,31 @@ export function CreateUser({ page }) {
       familyName,
     };
 
-    for (let key in familyMember) {
-      if (key !== "" && allFieldsFilled === false) {
-        console.log(key, allFieldsFilled);
-        setAllFieldsFilled(true);
-      }
-    }
-
-    if (familyMember.familyId !== "") {
-      if (!allFieldsFilled) {
-        console.log(familyMember.familyId);
-        fetch(
-          "http://localhost:8080/familymember/add?familyId=" +
-            familyMember.familyId,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(familyMember),
-          }
-        )
-          .then((res) => res.json())
-          .then((data) => setResponse(data));
-      }
+    if (!hasError && familyId) {
+      console.log(familyMember.familyId);
+      fetch(
+        "http://localhost:8080/familymember/add?familyId=" +
+          familyMember.familyId,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(familyMember),
+        }
+      )
+        .then((res) => res.text())
+        .then((data) => setResponse(data));
     }
   };
+  // console.log(response.status);
   useEffect(() => {
-    if (response.id) {
+    console.log(response);
+    if (response === "Created") {
       navigate({
         pathname: "/",
       });
-    } else {
-      setErrors(response);
     }
   }, [response]);
-  console.log(errors);
+  // console.log(errors);
 
   const handleNewFamily = (e) => {
     setNeedFamilyId(true);
@@ -90,8 +108,8 @@ export function CreateUser({ page }) {
   return (
     <div>
       <Navbar page={page} />
-      {errors && <ErrorMessage errors={errors} />}
-      {allFieldsFilled && <h2>Please fill out every field</h2>}
+      {/* {errors && <ErrorMessage errors={errors} />} */}
+
       <Container className="FamilyMember-form">
         <h1>New User</h1>
         <Box
@@ -104,11 +122,18 @@ export function CreateUser({ page }) {
         >
           <div>
             <TextField
+              className="Mui-required"
               required
               id="outlined-required"
               label="Your Email Address"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              error={getEmailValidationMessage(email)}
+              helperText={
+                getEmailValidationMessage(email)
+                  ? getEmailValidationMessage(email)
+                  : " "
+              }
             />
             <TextField
               required
@@ -116,6 +141,12 @@ export function CreateUser({ page }) {
               label="Your First Name"
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
+              error={getFieldValidationMessage(firstName)}
+              helperText={
+                getFieldValidationMessage(firstName)
+                  ? getFieldValidationMessage(firstName)
+                  : " "
+              }
             />
             <TextField
               required
@@ -123,6 +154,8 @@ export function CreateUser({ page }) {
               label="Your Last Name"
               value={lastName}
               onChange={(e) => setLastName(e.target.value)}
+              error={formSubmitted && lastName === ""}
+              helperText={formSubmitted && lastName === "" ? "Empty!" : " "}
             />
             <TextField
               required
@@ -130,6 +163,8 @@ export function CreateUser({ page }) {
               label="Username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
+              error={formSubmitted && username === ""}
+              helperText={formSubmitted && username === "" ? "Empty!" : " "}
             />
             <TextField
               required
@@ -139,6 +174,8 @@ export function CreateUser({ page }) {
               placeholder="Your password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              error={formSubmitted && password === ""}
+              helperText={formSubmitted && password === "" ? "Empty!" : " "}
             />
             <TextField
               required
@@ -146,6 +183,8 @@ export function CreateUser({ page }) {
               label="Mom's First Name"
               value={momFirstName}
               onChange={(e) => setMomFirstName(e.target.value)}
+              error={formSubmitted && momFirstName === ""}
+              helperText={formSubmitted && momFirstName === "" ? "Empty!" : " "}
             />
 
             <TextField
@@ -154,6 +193,8 @@ export function CreateUser({ page }) {
               label="Mom's Last Name"
               value={momLastName}
               onChange={(e) => setMomLastName(e.target.value)}
+              error={formSubmitted && momLastName === ""}
+              helperText={formSubmitted && momLastName === "" ? "Empty!" : " "}
             />
             <TextField
               required
@@ -161,6 +202,8 @@ export function CreateUser({ page }) {
               label="Dad's first Name"
               value={dadFirstName}
               onChange={(e) => setDadFirstName(e.target.value)}
+              error={formSubmitted && dadFirstName === ""}
+              helperText={formSubmitted && dadFirstName === "" ? "Empty!" : " "}
             />
             <TextField
               required
@@ -168,6 +211,8 @@ export function CreateUser({ page }) {
               label="Dad's Last Name"
               value={dadLastName}
               onChange={(e) => setDadLastName(e.target.value)}
+              error={formSubmitted && dadLastName === ""}
+              helperText={formSubmitted && dadLastName === "" ? "Empty!" : " "}
             />
             <TextField
               required
@@ -175,6 +220,8 @@ export function CreateUser({ page }) {
               label="Your Family ID"
               value={familyId}
               onChange={(e) => setFamilyId(e.target.value)}
+              error={formSubmitted && familyId === ""}
+              helperText={formSubmitted && familyId === "" ? "Empty!" : " "}
             />
             <div>
               {needFamilyId === true && (
@@ -183,6 +230,10 @@ export function CreateUser({ page }) {
                   label="Your Family Name"
                   value={familyName}
                   onChange={(e) => setFamilyName(e.target.value)}
+                  error={formSubmitted && familyName === ""}
+                  helperText={
+                    formSubmitted && familyName === "" ? "Empty!" : " "
+                  }
                 />
               )}
             </div>
